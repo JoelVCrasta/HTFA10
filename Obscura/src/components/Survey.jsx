24d6f23f5ce1b201c288c82dcc8808390e63997c
Rayan Pinto <22b03.joelc@sjec.ac.in>
2024-02-20 03:53:24 +0530

    changes

--- Obscura/src/components/Survey.jsx
+++ Obscura/src/components/Survey.jsx
@@ -1,11 +1,51 @@
-import React from 'react';
-import {Text, View, StyleSheet, SafeAreaView} from 'react-native';
+import React, {useState} from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  Button,
+  SafeAreaView,
+} from 'react-native';
+import supabase from 'Obscura/src/supabaseClient.js';
 
 const Survey = () => {
+  const [name, setName] = useState('');
+
+  const sendData = async () => {
+    try {
+      const {data, error} = await supabase
+        .from('obscure')
+        .insert([{name}])
+        .select();
+
+      if (error) throw error;
+    } catch (error) {
+      console.log('Error', error.message);
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <View>
         <Text style={styles.text}>Survey</Text>
+      </View>
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <TextInput
+          style={{
+            borderWidth: 1,
+            borderColor: 'gray',
+            padding: 10,
+            marginBottom: 10,
+            width: '80%',
+            height: 40,
+            color: 'white',
+          }}
+          placeholder="Enter your name"
+          defaultValue={name}
+          onChangeText={name => setName(name)}
+        />
+        <Button title="Submit" onPress={sendData} />
       </View>
     </SafeAreaView>
   );
@@ -13,6 +53,7 @@ const Survey = () => {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     height: 'auto',
   },
   text: {
