e735b2d2ed9700dccdd8d6c11106425e1c761200
Rayan Pinto <22b03.joelc@sjec.ac.in>
2024-02-21 01:01:31 +0530

    added text recognition and tab navigation

--- Obscura/App.jsx
+++ Obscura/App.jsx
@@ -1,14 +1,50 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
+import {Image, SafeAreaView, View, StyleSheet} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
 import Home from './src/screens/Home.jsx';
 import Ocr from './src/screens/Ocr.jsx';
 
+const Tab = createBottomTabNavigator();
+
 const App = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      {/* <Home /> */}
-      <Ocr />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarActiveTintColor: '#ba181b',
+          tabBarInactiveTintColor: 'gray',
+          tabBarStyle: {
+            height: 60,
+            paddingBottom: 10,
+          },
+        }}>
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarIconStyle: {display: 'none'},
+            tabBarLabelStyle: {
+              fontSize: 20,
+              marginBottom: 6,
+              fontWeight: 'bold',
+            },
+          }}
+        />
+        <Tab.Screen
+          name="OCR"
+          component={Ocr}
+          options={{
+            tabBarIconStyle: {display: 'none'},
+            tabBarLabelStyle: {
+              fontSize: 20,
+              marginBottom: 6,
+              fontWeight: 'bold',
+            },
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 };
 
--- Obscura/src/screens/Ocr.jsx
+++ Obscura/src/screens/Ocr.jsx
@@ -1,91 +1,111 @@
-  import React, {useState} from 'react';
-  import {
-    SafeAreaView,
-    Text,
-    StyleSheet,
-    View,
-    TouchableOpacity,
-  } from 'react-native';
-  import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+import React, {useEffect, useState} from 'react';
+import {
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+import {launchImageLibrary} from 'react-native-image-picker';
+import MlkitOcr from 'react-native-mlkit-ocr';
 
-  const Ocr = () => {
-    const [image, setImage] = useState(null);
+const Ocr = () => {
+  const [image, setImage] = useState(null);
 
-    const pickImage = async () => {
-      try {
-        const options = {
-          mediaType: 'photo',
-          maxWidth: 300,
-          maxHeight: 300,
-          quality: 1,
-          includeBase64: true,
-          saveToPhotos: true,
-        };
+  const pickImage = async () => {
+    try {
+      const options = {
+        mediaType: 'photo',
+        maxWidth: 2000,
+        maxHeight: 2000,
+        quality: 1,
+        includeBase64: true,
+        saveToPhotos: true,
+      };
 
-        const result = await launchImageLibrary(options);
-        console.log(result);
+      const result = await launchImageLibrary(options);
+      //console.log(result.assets[0].uri);
 
-        if (result.didCancel) {
-          console.log('User cancelled image picker');
-        } else if (result.error) {
-          console.log('ImagePicker Error: ', result.error);
-        } else {
-          setImage(result.uri);
-          console.log(image);
-        }
-      } catch (e) {
-        console.log(e);
+      if (result.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (result.error) {
+        console.log('ImagePicker Error: ', result.error);
+      } else {
+        setImage(result.assets[0].uri);
+        ocrDetect(result.assets[0].uri);
       }
-    };
-
-    return (
-      <SafeAreaView style={styles.container}>
-        <View>
-          <Text style={styles.header}>OCR</Text>
-        </View>
-        <View style={styles.box}>
-          <TouchableOpacity style={styles.press} onPress={pickImage}>
-            <Text style={styles.text}>Open Camera</Text>
-          </TouchableOpacity>
-        </View>
-      </SafeAreaView>
-    );
+    } catch (e) {
+      console.log(e);
+    }
   };
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-    },
-    box: {
-      width: '100%',
-      alignItems: 'center',
-      justifyContent: 'center',
-      marginTop: 150,
-    },
-    press: {
-      backgroundColor: '#ba181b',
-      padding: 10,
-      paddingRight: 20,
-      paddingLeft: 20,
-      borderRadius: 5,
-      alignItems: 'center',
-      fontSize: 40,
-    },
-    text: {
-      color: 'white',
-      fontWeight: 'bold',
-      fontSize: 20,
-    },
-    header: {
-      textAlign: 'center',
-      alignItems: 'center',
-      fontSize: 30,
-      fontWeight: 'bold',
-      color: 'white',
-      backgroundColor: '#ba181b',
-      paddingTop: 10,
-      paddingBottom: 10,
-    },
-  });
+  async function ocrDetect(imageUri) {
+    try {
+      const resultFromUri = await MlkitOcr.detectFromUri(imageUri);
+      let extractedText = '';
+      if (Array.isArray(resultFromUri)) {
+        resultFromUri.forEach(block => {
+          extractedText += block.text + ' ';
+        });
+      } else {
+        console.log('Unexpected structure of OCR result:', resultFromUri);
+      }
 
-  export default Ocr;
+      console.log(extractedText);
+      //console.log(resultFromUri);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={styles.header}>OCR</Text>
+      </View>
+      <View style={styles.box}>
+        <TouchableOpacity style={styles.press} onPress={pickImage}>
+          <Text style={styles.text}>Open Camera</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  box: {
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 150,
+  },
+  press: {
+    backgroundColor: '#ba181b',
+    padding: 10,
+    paddingRight: 20,
+    paddingLeft: 20,
+    borderRadius: 5,
+    alignItems: 'center',
+    fontSize: 40,
+  },
+  text: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  header: {
+    textAlign: 'center',
+    alignItems: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'white',
+    backgroundColor: '#ba181b',
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+});
+
+export default Ocr;
